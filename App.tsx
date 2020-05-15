7780f26e9a8948ae5184bd03a45b7cfd9922b064
OHMURA Tokiya <tocky.dison@gmail.com>
2020-05-15 11:17:45 +0900

    Fixed Error Boundaries

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import Navigator from './src/Navigation';
+import ErrorBoundary from './ErrorBoundary'
 
 const App: React.FunctionComponent = () => {
   return (
-    <Navigator />
+    <ErrorBoundary>
+      <Navigator />
+    </ErrorBoundary>
   );
 };
 
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './Screen/Home'
 import AddScreen from './Screen/Add'
-import ErrorBoundary from '../ErrorBoundary'
 
 type StackParamList = {
   Home: undefined,
@@ -16,12 +15,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" headerMode="none">
-        <ErrorBoundary>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <Stack.Screen name="Add" component={AddScreen} />
-        </ErrorBoundary>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
