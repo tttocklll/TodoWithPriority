ebcb6e182ad369ed1945822df6cca073b09837b1
OHMURA Tokiya <tocky.dison@gmail.com>
2020-05-16 13:29:47 +0900

    Removed AddScreen from navigation.

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './Screen/Home'
-import AddScreen from './Screen/Add'
+// import AddScreen from './Screen/Add'
 
 type StackParamList = {
   Home: undefined,
-  Add: undefined
+  // Add: undefined
 }
 
 const Stack = createStackNavigator<StackParamList>();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" headerMode="none" mode="modal"  >
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Add" component={AddScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
